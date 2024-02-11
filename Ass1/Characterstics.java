a6e6ffa42cd44afadab5bd46ad4782ec07d21858
AnkitMeher055 <159680369+AnkitMeher055@users.noreply.github.com>
2024-02-11 22:14:25 +0530

    Add files via upload

---
+++ Ass1/Characterstics.java
@@ -0,0 +1,12 @@
+public class Characterstics
+{
+public static void main(String ar[])
+{
+System.out.println("Java characterstics:");
+System.out.println("Class");
+System.out.println("Polymorphism");
+System.out.println("Inheritance");
+System.out.println("Abstraction");
+System.out.println("Encapsulation");
+}
+}
