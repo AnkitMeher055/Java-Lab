119fdafecfb9f2cdd3ece6db9abde69ce0abd9fe
AnkitMeher055 <159680369+AnkitMeher055@users.noreply.github.com>
2024-02-11 16:58:13 +0530

    Update and rename Detail to Detail.java
    gfj

--- Ass1/Detail.java
+++ Ass1/Detail.java
@@ -1,6 +1,6 @@
 public class Detail
 {
-public static void main(String ar[])
+public static void main(String arg[])
 {
 String name = "Ankit Meher";
 System.out.println("Welcome"+Name+"to JAVA programming);
