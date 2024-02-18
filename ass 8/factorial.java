e9fc0db8b43739a3c9fb968f8d454dd2a11d93d7
AnkitMeher055 <159680369+AnkitMeher055@users.noreply.github.com>
2024-02-18 19:32:31 +0530

    Update factorial.java

--- ass 8/factorial.java
+++ ass 8/factorial.java
@@ -1,1 +1,20 @@
-
+import java.util.Scanner;
+class factorial 
+  {  
+    static int factorial(int n)
+    {    
+     if (n == 0)    
+       return 1;    
+     else    
+       return(n * factorial(n-1));    
+    }    
+    public static void main(String args[]){  
+        Scanner in =new Scanner(System.in);
+     int i,fact=1;  
+     int n1;
+     System.out.println("Enter the value of n:");
+     n1=in.nextInt();
+       fact= factorial(n1);   
+     System.out.println("Factorial of "+n1+" is: "+fact);    
+    }  
+   }  
